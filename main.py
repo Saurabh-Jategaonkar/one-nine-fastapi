60088250013c0bcb162826037067892938803651
Saurabh Jategaonkar <sj749@njit.edu>
2023-11-02 22:22:16 -0400

    mistral install api - done

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ from mangum import Mangum
 import requests
 from datetime import datetime
 
-class Book(BaseModel):
-    name: str
-    genre: Literal["fiction", "non-fiction"]
-    price: float
-    book_id: Optional[str] = uuid4().hex
 
 class Assistant(BaseModel):
     bot_name: str
@@ -24,55 +19,11 @@ class Assistant(BaseModel):
     lp_text: str
     model_name: str
 
-BOOKS_FILE = "books.json"
-BOOKS = []
-
-if os.path.exists(BOOKS_FILE):
-    with open(BOOKS_FILE, "r") as f:
-        BOOKS = json.load(f)
-
 app = FastAPI()
-handler = Mangum(app)
-
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my bookstore app!"}
-
-
-@app.get("/random-book")
-async def random_book():
-    return random.choice(BOOKS)
-
-@app.get("/list-books")
-async def list_books():
-    return {"books": BOOKS}
-
-@app.get("/book_by_index/{index}")
-async def book_by_index(index: int):
-    if index < len(BOOKS):
-        return BOOKS[index]
-    else:
-        raise HTTPException(404, f"Book index {index} out of range ({len(BOOKS)}).")
-
-@app.post("/add-book")
-async def add_book(book: Book):
-    book.book_id = uuid4().hex
-    json_book = jsonable_encoder(book)
-    BOOKS.append(json_book)
-
-    with open(BOOKS_FILE, "w") as f:
-        json.dump(BOOKS, f)
-
-    return {"book_id": book.book_id}
-
-@app.get("/get-book")
-async def get_book(book_id: str):
-    for book in BOOKS:
-        if book.book_id == book_id:
-            return book
-
-    raise HTTPException(404, f"Book ID {book_id} not found in database.")
+    return {"message": "Welcome to OneNine!"}
 
 @app.post("/install")
 async def install(assistant: Assistant):
