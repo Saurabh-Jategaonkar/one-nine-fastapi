37d8dd99b41616a94f11ac35ce658c09ee1d0413
Saurabh Jategaonkar <sj749@njit.edu>
2023-11-03 00:07:21 -0400

    added password and initial code for prompt api

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from fastapi import FastAPI, HTTPException
 import random
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from mangum import Mangum
 
 import requests
 from datetime import datetime
-
+from ctransformers import AutoModelForCausalLM, AutoConfig, Config
 
 class Assistant(BaseModel):
     bot_name: str
@@ -18,6 +17,11 @@ class Assistant(BaseModel):
     description: str
     lp_text: str
     model_name: str
+    password: str
+
+class Prompt(BaseModel):
+    prompt: str
+    password: str
 
 app = FastAPI()
 
@@ -32,30 +36,49 @@ async def install(assistant: Assistant):
         "org_name": assistant.org_name,
         "description": assistant.description,
         "lp_text": assistant.lp_text,
-        "model_name": assistant.model_name
+        "model_name": assistant.model_name,
+        "password": assistant.password
     }
     print(result)
+    if result['password'] == 'onenine_2349bjd':
+        if result['model_name'] == 'mistral':
+            url = "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf"
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+            output_folder = f"installed_models/{result['bot_name']}_{timestamp}"
+            output_file = os.path.join(output_folder, "mistral_model.gguf")
 
-    if result['model_name'] == 'mistral':
-        url = "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q4_K_M.gguf"
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        output_folder = f"installed_models/{result['bot_name']}_{timestamp}"
-        output_file = os.path.join(output_folder, "mistral_model.gguf")
+            os.makedirs(output_folder, exist_ok=False)
+            
+            response = requests.get(url)
 
-        os.makedirs(output_folder, exist_ok=False)
-        
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            with open(output_file, 'wb') as file:
-                file.write(response.content)
-            print(f"File {output_file} downloaded successfully.")
-            result['is_downloaded'] = f"File {output_file} downloaded successfully."
-            result['bot_path'] = output_folder + "/mistral_model.gguf"
+            if response.status_code == 200:
+                with open(output_file, 'wb') as file:
+                    file.write(response.content)
+                print(f"Model downloaded successfully.")
+                result['is_downloaded'] = f"Model {output_file} downloaded successfully."
+                result['bot_path'] = output_folder + "/mistral_model.gguf"
+            else:
+                print(f"Failed to download file.")
+                result['is_downloaded'] = f"Failed to download file. Status code: {response.status_code}"
+                result['bot_path'] = None
         else:
-            print(f"Failed to download file. Status code: {response.status_code}")
-            result['is_downloaded'] = f"Failed to download file. Status code: {response.status_code}"
-            result['bot_path'] = None
+            return {'message': 'Please select a valid model.'}
     else:
-        return {'message': 'Please select a valid model.'}
+        return {'message': 'Not authorized. Re-check the password.'}
     return result
+
+@app.post("/prompt")
+async def prompt(promptInput: Prompt):
+    conf = AutoConfig(Config(temperature=0.8, repetition_penalty=1.1,
+                            batch_size=52, max_new_tokens=1024,
+                            context_length=2048))
+    llm = AutoModelForCausalLM.from_pretrained("mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+            model_type="mistral", config = conf)
+    
+    while(promptInput['prompt']!="exit"):
+        mistral_prompt = f"<s>[INST] {prompt} [/INST]"
+        answer = llm(mistral_prompt, temperature = 0.7,
+                repetition_penalty = 1.15,
+                max_new_tokens = 2048)
+        print(answer)
+        prompt = str(input("prompt"))
